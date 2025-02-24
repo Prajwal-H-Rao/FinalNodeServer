afb0af440399c7d49cf14ddf4917ae85efe601ff
Prajwal-H-Rao <prajwalhr130@gmail.com>
2025-02-24 12:00:35 +0530

    pack

--- scripts/download-ffmpeg.js
+++
@@ -1,4 +0,0 @@
-// Minimal script to satisfy postinstall requirement
-console.log("Downloading ffmpeg...");
-// ...insert any actual download logic if needed...
-console.log("ffmpeg installation is handled by the Dockerfile.");
